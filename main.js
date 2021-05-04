46e7e0c0f461c7a0b918a0d1d5109977252f5361
error404-sp <shreyasipatil404@gmail.com>
2021-05-04 09:05:58 +0530

    completed rock paper scissor

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-function computerPlay(){
+function computerPlay(){   //to generate random choices as Computer
     let gameOptions = ["Rock","Paper","Scissor"];
     let a = Math.floor(Math.random()*3);
-    console.log(gameOptions[a]);
+  
     return gameOptions[a];
 }
 let score = 0;
 
-function playRound(playerSelection,computerSelection){
+function playRound(playerSelection,computerSelection){   //to determine result of the round
+   
     if(playerSelection==computerSelection){
         console.log("It's a Tie!");
         return "It's a Tie!";
     }
-    else if((playerSelection == "Rock" && computerSelection == "Paper")
-        || (playerSelection == "Paper" && computerSelection == "Scissor") ||
-        (playerSelection == "Scissor" && computerSelection == "Rock") ){
+    else if((playerSelection.toLowerCase() == "Rock".toLowerCase()  && computerSelection.toLowerCase() == "Paper".toLowerCase())
+        || (playerSelection.toLowerCase() == "Paper".toLowerCase() && computerSelection.toLowerCase() == "Scissor".toLowerCase()) ||
+        (playerSelection == "Scissor".toLowerCase() && computerSelection == "Rock".toLowerCase()) ){
             
             console.log("You Lost! Try again")
             return "You Lost! Try again";
@@ -26,22 +27,23 @@ function playRound(playerSelection,computerSelection){
     }    
     
 }
-computerSelection = computerPlay();
-playerSelection = "Scissor"
 
+// to play 5 rounds
+for(i=0;i<5;i++){
+    playRound(prompt("Rock,Paper or Scissor??","Rock"),computerPlay()); 
+   
+ }
+ console.log(score);
 
-function game(){
-  for(i=0;i<5;i++){
-     playRound(playerSelection,computerSelection);
-    
-  }
-  console.log(score);
+function game(){  //to determine result of game
+  
+  
    if(score>5-score){
        return "You won the game";
    }
    else{
-       return "You lost the game!"
+       return "You lost the game!";
    }
 }
-
+console.log(game());
 
